8c3f4410a5c614ef95bf071068cd12402b5a7978
Dylan <dylanmmartin84@gmail.com>
2018-05-16 23:29:40 -0500

    Wow, I should have commited more often

---
+++ scripts/index.js
@@ -0,0 +1,57 @@
+function openNavMenu() {
+  console.log('Nav functionality coming soon!');
+}
+
+function getNavButton() {
+  if (window.pageYOffset >= 200) {
+    const elem = document.querySelector(".fixed__nav");
+    elem.style.opacity = 1;
+    elem.style.cursor = "pointer";
+    document.removeEventListener("scroll", getNavButton);
+    document.addEventListener("scroll", removeNavButton);
+  }
+}
+
+function removeNavButton() {
+  if (window.pageYOffset <= 200) {
+    const elem = document.querySelector(".fixed__nav");
+    elem.style.opacity = 0;
+    elem.style.cursor = "default";
+    document.removeEventListener("scroll", removeNavButton);
+    document.addEventListener("scroll", getNavButton);
+  }
+}
+
+document.addEventListener("scroll", getNavButton);
+
+function scrollToTop() {
+  if (window.pageYOffset >= 200) {
+    window.scroll({
+      left: 0,
+      top: 0,
+      behavior: "smooth"
+    });
+  }
+}
+
+function getScrollButton() {
+  if (window.pageYOffset >= 200) {
+    const elem = document.querySelector(".fixed__to-top");
+    elem.style.opacity = 1;
+    elem.style.cursor = "pointer";
+    document.removeEventListener("scroll", getScrollButton);
+    document.addEventListener("scroll", removeScrollButton);
+  }
+}
+
+function removeScrollButton() {
+  if (window.pageYOffset <= 200) {
+    const elem = document.querySelector(".fixed__to-top");
+    elem.style.opacity = 0;
+    elem.style.cursor = "default";
+    document.removeEventListener("scroll", removeScrollButton);
+    document.addEventListener("scroll", getScrollButton);
+  }
+}
+
+document.addEventListener("scroll", getScrollButton);
